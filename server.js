4f38312657511c58206a7b5c946d378914383f33
prathambande <pratham0802@gmail.com>
2022-11-13 12:28:10 +0530

    changed origins of backend

--- server.js
+++ server.js
@@ -22,13 +22,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   origin: true,
 // };
 // app.use(cors(corsConfig));
-app.use(
-  cors({
-    origin: "https://www.technozion.in",
-    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://www.technozion.in",
+//     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+//     credentials: true,
+//   })
+// );
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.technozion.in');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(cookieParser());
 app.use("/api/auth", user);
 app.use("/api/blog", require("./routes/blogs"));
