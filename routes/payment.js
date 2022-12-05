b2a3e322aa4388410ff7c854dba63dba41ec0e15
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2022-12-05 21:42:15 +0530

    Added routes for transactions

--- routes/payment.js
+++ routes/payment.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { checkout, paymentVerification } = require("../controller/payment.js");
+const { checkout, paymentVerification, addTransaction, manualPaymentVerification, getAllTransactions } = require("../controller/payment.js");
 
 const router = express.Router();
 
 router.route("/checkout").post(checkout);
 
 router.route("/paymentverification").post(paymentVerification);
+
+router.route("/addTransaction").post(addTransaction);
+
+router.route("/manualPaymentVerification").post(manualPaymentVerification);
+
+router.route("/getAllTransactions").get(getAllTransactions);
 
 module.exports = router;
