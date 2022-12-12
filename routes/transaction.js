58d9442693f7fe4c76bd10b9d4ac3dbe8e15a68a
Sufiyan Ansari <ansari_841903@student.nitw.ac.in>
2022-12-13 00:54:28 +0530

    Added routes for referral

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const route = express.Router();
 
-const { addTransaction, manualPaymentVerification, getAllTransactions } = require("../controller/payment.js");
+const { addTransaction, manualPaymentVerification, getAllTransactions, addReferralCodes } = require("../controller/payment.js");
 
 // MIDDLEWARE
 const {
-  isSignedIn,
+  isSignedIn, isTreasury,
 } = require("../middleware/auth");
 
-route.post("/addTransaction",isSignedIn, addTransaction);
+route.post("/addTransaction", isSignedIn, addTransaction);
 
-route.post("/manualPaymentVerification", manualPaymentVerification);
+route.post("/manualPaymentVerification", isSignedIn, isTreasury, manualPaymentVerification);
 
 route.get("/getAllTransactions", getAllTransactions);
+
+// route.post("/addReferralCodes", addReferralCodes)
 
 module.exports = route;
